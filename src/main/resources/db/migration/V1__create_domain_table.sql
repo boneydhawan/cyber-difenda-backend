4a8852e860732fb3aa9addac58ba84910d128707
boneydhawan <boneydhawan1995@gmail.com>
2025-08-31 11:28:41 -0500

    Update V1__create_domain_table.sql

--- src/main/resources/db/migration/V1__create_domain_table.sql
+++ src/main/resources/db/migration/V1__create_domain_table.sql
@@ -8,13 +8,3 @@ CREATE TABLE domain (
     is_active BOOLEAN NOT NULL
 );
 
--- Insert sample data
-INSERT INTO domain (domain_name, status, lastScan, issues, is_active) VALUES
-('example.com', 'Warning', '2m ago', '3', true),
-('testsite.org', 'Safe', '1h ago', '0', false),
-('secureapp.net', 'Critical', '5m ago', '7', true),
-('banksecure.io', 'Safe', '10m ago', '1', true),
-('myblog.dev', 'Warning', '30m ago', '2', false),
-('shopnow.store', 'Safe', '5h ago', '0', true),
-('travelbuddy.co', 'Critical', '15m ago', '5', true),
-('cybersecure.ai', 'Safe', '20m ago', '0', true);
